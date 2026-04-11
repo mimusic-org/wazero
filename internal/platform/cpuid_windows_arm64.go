077fe36f779c1dc170f6d453d615c6af9d03dcef
Nuno Cruces <ncruces@users.noreply.github.com>
2026-04-12 00:41:02 +0100

    Update golang.org/x/sys (#2486)
    Also:
    - updates minimum Go to 1.25, as per the policy defined in #2434
    - tests using real RISC-V hardware
    - tests using Go 1.26 in CI
    
    ---------
    
    Signed-off-by: Nuno Cruces <ncruces@users.noreply.github.com>

--- internal/platform/cpuid_windows_arm64.go
+++ internal/platform/cpuid_windows_arm64.go
@@ -1,14 +1,14 @@
-//go:build !windows
-
 package platform
 
-import "golang.org/x/sys/cpu"
+import "golang.org/x/sys/windows"
 
 // CpuFeatures exposes the capabilities for this CPU, queried via the Has method.
 var CpuFeatures = loadCpuFeatureFlags()
 
 func loadCpuFeatureFlags() (flags CpuFeatureFlags) {
-	if cpu.ARM64.HasATOMICS {
+	// See: go.dev/cl/757482
+	// golang.org/x/sys/cpu can't import golang.org/x/sys/windows and can't do this yet.
+	if windows.IsProcessorFeaturePresent(windows.PF_ARM_V81_ATOMIC_INSTRUCTIONS_AVAILABLE) {
 		flags |= CpuFeatureArm64Atomic
 	}
 	return
